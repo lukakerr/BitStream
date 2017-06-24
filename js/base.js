c276cb6bd3e1619f9382f3afaab6948c8949a928
LukaKerr <lukakerr1@gmail.com>
2017-06-24 16:08:36 +1000

    MP3 Support, better layout

--- js/base.js
+++ js/base.js
@@ -6,41 +6,60 @@ client.on('error', function (err) {
 	console.error('Error: ' + err.message)
 })
 
+// Listen for for input
 document.querySelector('form').addEventListener('submit', function (e) {
 	e.preventDefault()
 
+	// Clear out contents
+	document.getElementById('title').innerHTML = '';
+	document.getElementById('percentage').innerHTML = '';
+	document.querySelector('.output').innerHTML = '';
+
+	// Get magnet
 	var torrentId = document.querySelector('form input[name=magnet]').value
+	// Show loading spinner
 	loading.style.display = 'block';
-	document.getElementById('status').innerHTML = 'Adding file...';
+	// Start downloading torrent, callback to onTorrent
 	client.add(torrentId, { path: __dirname + '/Downloads' }, onTorrent)
 })
 
 function onTorrent(torrent) {
-	document.getElementById('status').innerHTML = 'Downloading & Streaming Now... Enjoy Your Movie';
-	loading.style.display = 'none';
-
-	setTimeout(function () {
-		document.getElementById('status').style.display = "none";
-	}, 5000);
-
-	var file = torrent.files.find(function (file) {
-		return file.name.endsWith('.mp4')
+	// Iterate over each file in torrent
+	torrent.files.forEach(function (file) {
+		// If .mp3
+		if (file.name.endsWith('.mp3')) {
+			// Add file name to title element and append file to output - dont autoplay audio as there's usually multiple songs
+			log(file.name)
+			file.appendTo('.output', { autoplay: false })
+		// If .mp4
+		} else if (file.name.endsWith('.mp4')) {
+			// Add file name to title element and append file to output
+			log(file.name)
+			file.appendTo('.output')
+		}
+		file.getBlobURL(function (err, url) {
+			if (err) return log(err.message)
+		})
 	})
 
-	file.appendTo('.output')
+	// Hide loading spinner
+	loading.style.display = 'none';
 
+	// Every 5 seconds get download percentage
 	var interval = setInterval(function () {
-		document.getElementById('percentage').innerHTML = 'Progress: ' + (torrent.progress * 100).toFixed(1) + '%'
+		document.getElementById('percentage').innerHTML = 'Downloading... ' + (torrent.progress * 100).toFixed(1) + '%'
 	}, 5000)
 
+	// When torrent is done, clear the interval
 	torrent.on('done', function () {
-		ldocument.getElementById('percentage').innerHTML = 'Progress: 100%'
+		document.getElementById('percentage').innerHTML = 'Progress: 100%'
 		clearInterval(interval)
 	})
 }
 
-function log (str) {
+function log(str) {
 	var p = document.createElement('p')
+	p.setAttribute("id", "title")
 	p.innerHTML = str
 	document.querySelector('.output').appendChild(p)
 }
